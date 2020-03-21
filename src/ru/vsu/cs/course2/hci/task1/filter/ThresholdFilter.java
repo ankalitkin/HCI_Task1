4d4144f2be37aa105b1b5b2cff90c602bf5673ff
Andrey Kalitkin <ankalitkin@yandex.ru>
2020-03-22 00:53:37 +0300

    ThresholdFilterChanged

--- src/ru/vsu/cs/course2/hci/task1/filter/ThresholdFilter.java
+++ src/ru/vsu/cs/course2/hci/task1/filter/ThresholdFilter.java
@@ -2,9 +2,17 @@ package ru.vsu.cs.course2.hci.task1.filter;
 
 public class ThresholdFilter implements Filter {
     private final int threshold;
+    private final int low;
+    private final int high;
+
+    public ThresholdFilter(int threshold, int low, int high) {
+        this.threshold = threshold;
+        this.low = low;
+        this.high = high;
+    }
 
     public ThresholdFilter(int threshold) {
-        this.threshold = threshold;
+        this(threshold, 0, 100);
     }
 
     @Override
@@ -16,7 +24,7 @@ public class ThresholdFilter implements Filter {
     public double[] doFilter(double[] source) {
         double[] res = new double[source.length];
         for (int i = 0; i < source.length; i++) {
-            res[i] = (source[i] >= threshold) ? threshold : 0;
+            res[i] = (source[i] >= threshold) ? high : low;
         }
         return res;
     }
